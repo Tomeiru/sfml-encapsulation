d8b3ac3730fd08bdc5af289dd5eafb69f41c13ac
Mathieu Brujan <mathieu.brujan@epitech.eu>
2023-01-28 12:30:49 +0100

    feat: add SpriteManager, responsible of Sprites' creation and linking to ID

---
+++ SpriteManager.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+
+#include <unordered_map>
+#include <string>
+#include <memory>
+#include "Sprite.hpp"
+
+namespace SFML {
+    class SpriteManager {
+    public:
+        void registerSprite(const std::string &id, const std::shared_ptr<Texture> &texture) {
+            if (sprites.find(id) != sprites.end())
+                throw RuntimeException("SpriteManager::registerSprite", "A texture with this ID has already been registered");
+            std::shared_ptr<Sprite> sprite = std::make_shared<Sprite>(texture);
+            sprites.emplace(id, sprite);
+        }
+
+        std::shared_ptr<Sprite> getSprite(const std::string &id) {
+            if (sprites.find(id) == sprites.end())
+                throw RuntimeException("SpriteManager::getSprite", "The ID provided doesn't match with any texture previously registered");
+            return sprites.at(id);
+        }
+    private:
+        std::unordered_map<std::string, std::shared_ptr<Sprite>> sprites;
+    };
+}
