a749007e022005dc4e895228450e363deb3a9786
Tomeiru <71699948+Tomeiru@users.noreply.github.com>
2023-02-05 16:03:21 +0000

    Deploying to gh-pages from @ Tomeiru/sfml-encapsulation@51c1526cd6e32b823141cb9593e5956749a0592f 🚀

--- search/all_4.js
+++ search/all_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['event_0',['Event',['../class_s_f_m_l_1_1_event.html#a49e4c1dee64919505f105e47030d3306',1,'SFML::Event::Event()'],['../class_s_f_m_l_1_1_event.html',1,'SFML::Event']]],
+  ['event_0',['Event',['../class_s_f_m_l_1_1_event.html#ad07d57490b35088d8d01ad9054b505e1',1,'SFML::Event::Event()'],['../class_s_f_m_l_1_1_event.html#a49e4c1dee64919505f105e47030d3306',1,'SFML::Event::Event(const sf::Event &amp;event)'],['../class_s_f_m_l_1_1_event.html',1,'SFML::Event']]],
   ['eventmanager_1',['EventManager',['../class_s_f_m_l_1_1_event_manager.html',1,'SFML']]],
   ['eventtype_2',['EventType',['../class_s_f_m_l_1_1_event.html#a69a482a6bce0315fdb6bcb6fde274664',1,'SFML::Event']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['event_0',['Event',['../class_s_f_m_l_1_1_event.html#a49e4c1dee64919505f105e47030d3306',1,'SFML::Event']]]
+  ['event_0',['Event',['../class_s_f_m_l_1_1_event.html#ad07d57490b35088d8d01ad9054b505e1',1,'SFML::Event::Event()'],['../class_s_f_m_l_1_1_event.html#a49e4c1dee64919505f105e47030d3306',1,'SFML::Event::Event(const sf::Event &amp;event)']]]
 ];
